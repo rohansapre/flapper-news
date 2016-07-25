b546e657efca35fdabb33af3c59975f732d56a16
rohansapre <11bit025@nirmauni.ac.in>
2016-07-25 19:17:27 +0530

    getting user input

---
+++ app.js
@@ -0,0 +1,27 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+	'$scope', 
+	function($scope){
+		$scope.test = 'Hello World!';
+		$scope.posts = [
+		{title: 'post1', upvotes: 5},
+		{title: 'post2', upvotes: 2},
+		{title: 'post3', upvotes: 15},
+		{title: 'post4', upvotes: 9},
+		{title: 'post5', upvotes: 4}
+		];
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === '') { return; }
+			$scope.posts.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0
+			});
+			$scope.title = '';
+			$scope.link = '';
+		};
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes += 1;
+		};
+}]);
